aaf9232a704c459a1b2e00e63f5d25330cd7c487
unknown <sarahhsaeed23@gmail.com>
2024-01-12 17:07:13 +0200

    last update

--- NTI_RTOS_MOTOR_USART_SELF_UPDATED_GP/Core/HCSR04/HCSR04_cfg.h
+++
@@ -1,8 +0,0 @@
-#ifndef HCSR04_CFG_H_
-#define HCSR04_CFG_H_
-
-#include "../HCSR04/HCSR04.h"
-
-extern const HCSR04_CfgType HCSR04_CfgParam[HCSR04_UNITS];
-
-#endif /* HCSR04_CFG_H_ */
--- NTI_RTOS_MOTOR_USART_SELF_UPDATED_GP/Core/SERVO/SERVO_cfg.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVO_CFG_H_
-#define SERVO_CFG_H_
-
-#include "SERVO.h"
-
-extern const SERVO_CfgType SERVO_CfgParam[SERVO_NUM];
-
-#endif /* SERVO_CFG_H_ */
